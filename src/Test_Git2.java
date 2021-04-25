7b07b359fd50a571d0ac6b175a0fb216c74f9c3a
Administrator <Administrator@Venkat_Lap>
2021-04-25 21:36:11 +0530

    Committing Test_Git2 into new Branch

---
+++ src/Test_Git2.java
@@ -0,0 +1,10 @@
+
+public class Test_Git2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing Git project");
+		System.out.println("Code Modification done here!");
+	}
+
+}
